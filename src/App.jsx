bbfa489eefe77565efed7e42c9b6c616d9efc156
Jorge Reyes <rjorge99@hotmail.com>
2023-03-01 10:22:43 -0600

    CustomHook

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './app.css';
 import HigherOrderComponent from './patterns/1_HigherOrderComponent';
+import MediumClapWithCustomHook from './patterns/2_CustomHook';
 
 function App() {
     return (
         <>
-            <HigherOrderComponent />
+            {/* <HigherOrderComponent /> */}
+            <MediumClapWithCustomHook />
         </>
     );
 }
