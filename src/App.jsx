f82a9ff6811ed1e62e8f5347d4633278f56db9bd
Jorge Reyes <rjorge99@hotmail.com>
2023-03-02 10:55:28 -0600

    ReusableStyles

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './app.css';
 import HigherOrderComponent from './patterns/1_HigherOrderComponent';
 import MediumClapWithCustomHook from './patterns/2_CustomHook';
 import CompoundComponent from './patterns/3_CompoundComponent';
+import ReusableStyle from './patterns/4_ReusableStyles';
 
 function App() {
     return (
         <>
             {/* <HigherOrderComponent /> */}
             {/* <MediumClapWithCustomHook /> */}
-            <CompoundComponent />
+            {/* <CompoundComponent /> */}
+            <ReusableStyle />
         </>
     );
 }
